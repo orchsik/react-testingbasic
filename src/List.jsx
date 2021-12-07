cf8e47fc73b9af1015216c30bcab68187aee400f
orchsik <orchsik@naver.com>
2021-12-07 21:45:26 +0900

    Add: List 컴포넌트

--- src/List.jsx
+++ src/List.jsx
@@ -1,1 +1,11 @@
- 
+import React from "react";
+
+export default function List({ tasks }) {
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={task.id}>{task.title}</li>;
+      })}
+    </ul>
+  );
+}
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,10 +1,17 @@
-import React from "react";
 import { render } from "@testing-library/react";
+import React from "react";
 import List from "./List";
 
 describe("List", () => {
   it("renders tasks", () => {
-    const { container } = render(<List />);
-    expect(container).toHaveTextContent("아무일도 하기 싫다.");
+    const tasks = [
+      { id: 1, title: "아무 일도 하기 싫다" },
+      { id: 2, title: "건물 매입" },
+    ];
+
+    const { container } = render(<List tasks={tasks} />);
+
+    expect(container).toHaveTextContent("아무 일도 하기 싫다");
+    expect(container).toHaveTextContent("건물 매입");
   });
 });
