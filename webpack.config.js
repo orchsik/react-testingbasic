baf2593092afcf4bf7f48535ccd4db8b6b9bbdf3
orchsik <orchsik@naver.com>
2021-12-07 21:11:08 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.jsx"),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    host: "127.0.0.1",
+    port: 3000,
+  },
+};
