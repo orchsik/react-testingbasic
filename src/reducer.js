1c3a4de68c0b6934f9d2353811e9f753caffe606
orchsik <orchsik@naver.com>
2021-12-07 22:02:19 +0900

    Add: redux todo

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,24 @@
-const initialState = {};
+const initialState = {
+  tasks: [],
+};
 
-export default function reducer(state = initialState, action) {}
+export default function reducer(state = initialState, action) {
+  if (action.type === "setTasks") {
+    const { tasks } = action.payload;
+    return {
+      ...state,
+      tasks,
+    };
+  }
+
+  if (action.type === "deleteTask") {
+    const { tasks } = state;
+
+    return {
+      ...state,
+      tasks: tasks.filter((task) => task.id !== action.payload.id),
+    };
+  }
+
+  return state;
+}
