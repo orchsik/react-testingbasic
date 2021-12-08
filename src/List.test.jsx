2a4e57e8c6ce4cd9d8d347ead8a5417d2acf9839
orchsik <orchsik@naver.com>
2021-12-08 21:10:41 +0900

    Add: List.test

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import List from "./List";
+import tasks from "../fixtures/tasks";
 
 describe("List", () => {
-  it("renders tasks", () => {
-    const tasks = [
-      { id: 1, title: "아무 일도 하기 싫다" },
-      { id: 2, title: "건물 매입" },
-    ];
+  context("with tasks", () => {
+    it("renders tasks", () => {
+      const { container } = render(<List tasks={tasks} />);
 
-    const { container } = render(<List tasks={tasks} />);
+      expect(container).toHaveTextContent("아무 것도 하지 않기");
+      expect(container).toHaveTextContent("건물 매입");
+    });
+  });
 
-    expect(container).toHaveTextContent("아무 일도 하기 싫다");
-    expect(container).toHaveTextContent("건물 매입");
+  context("without tasks", () => {
+    it("renders no task message", () => {
+      const tasks = [];
+      const { container } = render(<List tasks={tasks} />);
+
+      expect(container).toHaveTextContent("할 일이 없어요!");
+    });
   });
 });
