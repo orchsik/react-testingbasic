86023512063932f6c3007eae369bcaa0c9418f0f
orchsik <orchsik@naver.com>
2021-12-09 16:07:16 +0900

    Fix: setUpText.js

--- rtl-tutorial/src/setupTests.js
+++ rtl-tutorial/src/setupTests.js
@@ -2,4 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// 각 테스트 케이스가 끝날때마다 기존 가상 화면에 남아있는 UI 정리
+import "@testing-library/react/dont-cleanup-after-each";
