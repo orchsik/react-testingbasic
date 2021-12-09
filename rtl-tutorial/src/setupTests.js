0b0754023639731bc495637d46cf9c9298d4deff
orchsik <orchsik@naver.com>
2021-12-09 17:39:06 +0900

    Fix: test setup

--- rtl-tutorial/src/setupTests.js
+++ rtl-tutorial/src/setupTests.js
@@ -4,5 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
-// 각 테스트 케이스가 끝날때마다 기존 가상 화면에 남아있는 UI 정리
-import "@testing-library/react/dont-cleanup-after-each";
+// 각 테스트 케이스가 끝날때마다 기존 가상 화면에 남아있는 UI 정리 안함
+// import "@testing-library/react/dont-cleanup-after-each";
